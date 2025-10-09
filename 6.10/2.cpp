26dba3b721d80da6793b429abc0f269228cfb0c7
vivivoll <viktornosk0v404@gmail.com>
2025-10-09 20:58:11 +0900

    Add files via upload

---
+++ 6.10/2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	int input, output, minute;
+
+	cout << "С какого оператора будет проходить звонок?\n1.МТС,\n2.Мегафон\n";
+	cin >> input;
+
+	cout << "На какой оператор будет проходить звонок?\n1.МТС,\n2.Мегафон\n";
+	cin >> output;
+
+	cout << "Сколько минут будет длиться звонок?\n";
+	cin >> minute;
+
+
+	switch (input) {
+	case 1:
+		switch (output) {
+		case 1:
+			cout << "Звонок будет бесплатным";
+			break;
+		case 2:
+			cout << "Звонок составит: " << minute * 1.5 << " рублей";
+			break;
+		}
+		break;
+	case 2:
+		switch (output) {
+		case 1:
+			cout << "Звонок составит: " << minute * 2 << " рублей";
+			break;
+		case 2:
+			cout << "Звонок будет бесплатным";
+			break;
+		}
+		break;
+	}
+
+
+	return 0;
+}
+
+
+
+
---
+++ 6.10/project.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+int main() {
+	using namespace std;
+	setlocale(LC_ALL, "ru");
+
+	char symbol;
+	
+	cout << "Введите символ: ";
+	cin >> symbol;
+
+	switch (symbol) {
+
+		case '.': case ',': case '!':
+		case '?': case ':': case ';':
+
+			cout << "Знак препинания";
+			break;
+
+		case '1': case '2': case '3': case '4': case '5':
+		case '6': case '7': case '8': case '9': case '0':
+
+			cout << "Цифра";
+			break;
+
+		default:
+
+			if (symbol >= 'A' && symbol <= 'Z') {
+				cout << "Буква";
+			}
+			else if (symbol >= 'a' && symbol <= 'z') {
+				cout << "Буква";
+			}
+			else {
+				cout << "Другое";
+			}
+	}
+	return 0;
+}
