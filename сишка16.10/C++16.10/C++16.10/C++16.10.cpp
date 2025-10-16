64d500d784df5b702ab81a1fe5f1c0ee577e0c58
vivivoll <viktornosk0v404@gmail.com>
2025-10-16 12:50:57 +0900

    Add files via upload

---
+++ сишка16.10/C++16.10/C++16.10/C++16.10.cpp
@@ -0,0 +1,79 @@
+﻿/*#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int a, b, c;
+
+    cout << "Введите три числа: ";
+    cin >> a >> b >> c;
+
+    int max = a;
+
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+
+    cout << "Наибольшее число: " << max << endl;
+
+    return 0;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int n;
+
+    cout << "Введите размер таблицы: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << i * j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int x;
+    cout << "Введите число: ";
+    cin >> x;
+
+    if (x % 15 == 0) cout << "FizzBuzz";
+    else if (x % 3 == 0) cout << "Fizz";
+    else if (x % 5 == 0) cout << "Buzz";
+    else cout << x;
+
+    return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int n, f = 1;
+    cout << "Введите число: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        f = f * i;
+    }
+
+    cout << f;
+    return 0;
+}
