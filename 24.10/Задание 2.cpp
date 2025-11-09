7ba8767648a49840d2a8135ad215e8831eba6722
vivivoll <viktornosk0v404@gmail.com>
2025-11-09 17:00:15 +0900

    Add files via upload

---
+++ 24.10/Задание 2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	
+	int size;
+	cout << "Введите число - ";
+	cin >> size; 
+
+
+	const int row = 3;
+	const int col = 3;
+	int array[row][col];
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			cout << size << " ";
+			size += 1;
+		}
+		cout << "\n\n";
+	}
+
+
+	return 0;
+}
---
+++ 24.10/Задание 3.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	const int row1 = 3;
+	const int col1 = 3;
+	int array1[row1][col1];
+	for (int i = 0; i < row1; i++) {
+		for (int j = 0; j < col1; j++) {
+			array1[i][j] = rand() % 50;
+			cout << array1[i][j] << " ";
+		}
+		cout << "\n\n";
+	}
+
+	int sdvig;
+	cout << "Выберите насколько сдвинуть массив - ";
+	cin >> sdvig;
+
+
+	const int row2 = 3;
+	const int col2 = 3;
+	int array2[row2][col2];
+	for (int i = 0; i < row2; i++) {
+		for (int j = 0; j < col2; j++) {
+			array2[i][j] = rand() % 50;
+			array2[i][j] += sdvig;
+			cout << array2[i][j] << " ";
+		}
+		cout << "\n\n";
+	}
+
+
+
+
+	return 0;
+}
