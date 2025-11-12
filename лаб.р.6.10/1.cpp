b5446a81b8d65774f627df43538ef82b7567b7be
vivivoll <viktornosk0v404@gmail.com>
2025-11-12 21:14:09 +0900

    Add files via upload

---
+++ лаб.р.6.10/1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	int text, back;
+	cout << "Выберите цвет фона - ";
+	cin >> back;
+
+	cout << "Выберите цвет текста - ";
+	cin >> text;
+
+	SetConsoleTextAttribute(hConsole, (back << 4) | text);
+
+
+
+	return 0;
+}
