db5f5f89aa11e0159eff26031caf2a1bfcc642c6
vivivoll <viktornosk0v404@gmail.com>
2025-11-12 21:10:53 +0900

    Add files via upload

--- лаб6.10/1.cpp
+++ лаб6.10/1.cpp
@@ -1,21 +1,118 @@
 ﻿#include <iostream>
-#include <windows.h>
 using namespace std;
 
+enum pizza {
+    pizza1,
+    pizza2,
+    pizza3,
+    pizza4
+};
+
+enum drink {
+    drink1,
+    drink2,
+    drink3
+};
+
 int main() {
-	setlocale(LC_ALL, "ru");
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    setlocale(LC_ALL, "ru");
 
-	int text, back;
-	cout << "Выберите цвет фона - ";
-	cin >> back;
-
-	cout << "Выберите цвет текста - ";
-	cin >> text;
-
-	SetConsoleTextAttribute(hConsole, (back << 4) | text);
+    int pizzaCode, pizza_count;
+    int drinkCode, drink_count;
 
 
+    cout << "Меню:\nПиццы:\n";
+    cout << "pizza1 - $8\n";
+    cout << "pizza2 - $10\n";
+    cout << "pizza3 - $9\n";
+    cout << "pizza4 - $11\n";
 
-	return 0;
+    cout << "Напитки:\n";
+    cout << "drink1 - $1\n";
+    cout << "drink2 - $3\n";
+    cout << "drink3 - $2\n";
+
+
+    cout << "Выберите пиццу: ";
+    cin >> pizzaCode;
+    cout << "Введите количество: ";
+    cin >> pizza_count;
+
+ 
+    cout << "Выберите напиток: ";
+    cin >> drinkCode;
+    cout << "Введите количество: ";
+    cin >> drink_count;
+
+
+    int pizzaPrice;
+    string pizzaName;
+
+    switch (pizzaCode) {
+    case pizza1:
+        pizzaPrice = 8; 
+        pizzaName = "pizza1"; 
+        break;
+
+    case pizza2:
+        pizzaPrice = 10; 
+        pizzaName = "pizza2"; 
+        break;
+
+    case pizza3:
+        pizzaPrice = 9; 
+        pizzaName = "pizza3"; 
+        break;
+
+    case pizza4:
+        pizzaPrice = 11; 
+        pizzaName = "pizza4"; 
+        break;
+    }
+
+
+
+    int drinkPrice;
+    string drinkName;
+
+    switch (drinkCode) {
+    case drink1:
+        drinkPrice = 1; 
+        drinkName = "drink1";
+        break;
+
+    case drink2:
+        drinkPrice = 3; 
+        drinkName = "drink2"; 
+        break;
+
+    case drink3:
+        drinkPrice = 2; 
+        drinkName = "drink3"; 
+        break;
+    }
+
+
+    int FreePizza = pizza_count / 5;
+    int PayPizza = pizza_count - FreePizza;
+    int TotalPizza = PayPizza * pizzaPrice;
+
+
+    int DrinkTotal = drink_count * drinkPrice;
+    if (drinkPrice > 2 && drink_count > 3) {
+        DrinkTotal = DrinkTotal * 85 / 100;
+    }
+
+
+    
+    
+    int Total = TotalPizza + DrinkTotal;
+    if (Total > 50) {
+        Total = Total * 80 / 100;
+    }
+    
+    cout << "Пицца: " << pizzaName << " " << pizza_count << " шт. Цена: " << TotalPizza << "\n";
+    cout << "Напитки: " << pizzaName << " " << pizza_count << " шт. Цена: " << DrinkTotal << "\n";
+    cout << "Оплата: " << Total;
+
 }
--- лаб6.10/2.cpp
+++ лаб6.10/2.cpp
@@ -2,20 +2,73 @@
 using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "ru");
+    setlocale(LC_ALL, "ru");
 
-	int number;
-	cout << "Введите число - ";
-	cin >> number;
 
-	cout << "Число в 0 степени - " << 1 << "\n";
-	cout << "Число в 1 степени - " << number << "\n";
-	cout << "Число в 2 степени - " << number * number << "\n";
-	cout << "Число в 3 степени - " << number * number * number << "\n";
-	cout << "Число в 4 степени - " << number * number * number * number << "\n";
-	cout << "Число в 5 степени - " << number * number * number * number * number << "\n";
-	cout << "Число в 6 степени - " << number * number * number * number * number * number << "\n";
-	cout << "Число в 7 степени - " << number * number * number * number * number * number * number << "\n";
+    int first_manager, second_manager, third_manager;
+    int total_first_manager, total_second_manager, total_third_manager;
 
-	return 0;
+    cout << "Введите уровень продаж менеджера 1: ";
+    cin >> first_manager;
+
+    cout << "Введите уровень продаж менеджера 2: ";
+    cin >> second_manager;
+
+    cout << "Введите уровень продаж менеджера 3: ";
+    cin >> third_manager;
+
+    int money_manager = 200;
+
+   
+    if (first_manager <= 500)
+        total_first_manager = money_manager + first_manager * 3 / 100;
+    else if (first_manager <= 1000)
+        total_first_manager = money_manager + first_manager * 5 / 100;
+    else
+        total_first_manager = money_manager + first_manager * 8 / 100;
+
+   
+    if (second_manager <= 500)
+        total_second_manager = money_manager + second_manager * 3 / 100;
+    else if (second_manager <= 1000)
+        total_second_manager = money_manager + second_manager * 5 / 100;
+    else
+        total_second_manager = money_manager + second_manager * 8 / 100;
+
+    if (third_manager <= 500)
+        total_third_manager = money_manager + third_manager * 3 / 100;
+    else if (third_manager <= 1000)
+        total_third_manager = money_manager + third_manager * 5 / 100;
+    else
+        total_third_manager = money_manager + third_manager * 8 / 100;
+
+    
+    string best_manager_name;
+    int best_manager_total;
+
+    if (total_first_manager > total_second_manager && total_first_manager > total_third_manager) {
+        best_manager_name = "Менеджер 1";
+        total_first_manager += 200;
+        best_manager_total = total_first_manager;
+    }
+    else if (total_second_manager > total_first_manager && total_second_manager > total_third_manager) {
+        best_manager_name = "Менеджер 2";
+        total_second_manager += 200;
+        best_manager_total = total_second_manager;
+    }
+    else {
+        best_manager_name = "Менеджер 3";
+        total_third_manager += 200;
+        best_manager_total = total_third_manager;
+    }
+
+
+    cout << "Менеджер 1: " << total_first_manager << "\n";
+    cout << "Менеджер 2: " << total_second_manager << "\n";
+    cout << "Менеджер 3: " << total_third_manager << "\n";
+
+
+    cout << "Лучший менеджер: " << best_manager_name << ", итоговая сумма с премией: " << best_manager_total << "\n";
+
+    return 0;
 }
