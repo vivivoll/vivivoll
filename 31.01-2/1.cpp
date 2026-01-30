6b9104be53871f804143e3b6858e59ee62cd725f
vivivoll <viktornosk0v404@gmail.com>
2026-01-30 20:50:06 +0900

    Add files via upload

---
+++ 31.01-2/1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define max(a,b) ((a) > (b) ? (a) : (b)) //нахождения макс числа
+#define min(a,b) ((a) < (b) ? (a) : (b)) //нахождения мин числа
+#define square(a) ((a) * (a)) //возведения числа в квадрат
+#define degree(a, b) (pow((a), (b))) //возведения числа в степень
+#define parity(a) ((a) % 2 == 0) //проверка числа на четность
+#define inparity(a) ((a) % 2 != 0) //проверка числа на нечетность
+
+int main(){
+    setlocale(LC_ALL, "ru");
+
+    //для предпроцессора
+    int a = 10;
+    int b = 5;
+
+    //визуалка
+    cout << "Максимальное число: " << max(a,b) << endl;
+    cout << "Минимальное число: " << min(a,b) << endl;
+    cout << "Число " << a << " в квадрате: " << square(a) << endl;
+    cout << "Число " << a << " в " << b << " степени: " << degree(a, b) << endl;
+    cout << "Число " << a << ": " << (parity(a) ? "четное" : "нечетное") << endl;
+    cout << "Число " << b << ": " << (inparity(b) ? "нечетное" : "четное") << endl;
+
+    return 0;
+}
