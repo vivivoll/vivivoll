573173cf6906bf0cc86e1552e015cde234d4ca5e
vivivoll <viktornosk0v404@gmail.com>
2026-05-18 00:34:02 +0900

    Add files via upload

---
+++ 15.05-3/1-7.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleNew
+{
+    class Programs
+    {
+        enum ArticleType //Перечисление товара
+        {
+            Food,
+            Books,
+            Toys,
+        }
+
+        struct Article //Структура товара
+        {
+            public int code; //код товара
+            public string name; //название товара
+            public double price; //цена товара
+            public ArticleType Type; //Тип товара
+        }
+
+        enum ClientType //Перечесление клиентов
+        {
+            Normal,
+            Important,
+        }
+
+        struct Client //Структура клиента
+        {
+            public ClientType Type; //Тип клиента
+            public int code; //код товара
+            public string fullName; //ФИО
+            public string address; //адрес
+            public string phone; //телефон
+            public int spisokRequest; //количество заказов осуществленных клиентом
+            public int totalCountRequest; //общая сумма заказов клиента
+        }
+
+        struct RequestItem //Структура товара
+        {
+            public Article article; //товар
+            public int count_article; //количество единиц товара
+        }
+
+        enum PayType //Перечисление оплаты
+        {
+            Card,
+            Cash,
+        }
+
+        struct Request //Структура заказа
+        {
+            public PayType Type;
+            public int code_requestItem; //код заказа
+            public Client client; //клиент
+            public string date_request; //дата заказа
+            public RequestItem[] items; //перечень заказанных товаров
+            public double sum_request //сумма заказа (вычисляемое свойвтво)
+            {
+                get
+                {
+                    //сумма
+                    double total = 0;
+                    //цена * количество
+                    foreach (RequestItem item in items)
+                    {
+                        total += item.article.price * item.count_article;
+                    }
+                    return total;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Структура товара
+            Article article = new Article
+            {
+                code = 133,
+                name = "ПК",
+                price = 50000,
+            };
+            Console.WriteLine("Структура товара");
+            Console.WriteLine("Код: " + article.code);
+            Console.WriteLine("Название: " + article.name);
+            Console.WriteLine("Цена: " + article.price);
+            Console.WriteLine("");
+
+            //Структура клиента
+            Client client_1 = new Client
+            {
+                code = 455,
+                fullName = "Иванов Иван Иванович",
+                address = "Ленина 123",
+                phone = "88005553535",
+                spisokRequest = 2,
+                totalCountRequest = 13,
+            };
+            Console.WriteLine("Структура клиента");
+            Console.WriteLine("Код: " + client_1.code);
+            Console.WriteLine("ФИО: " + client_1.fullName);
+            Console.WriteLine("Телефон: " + client_1.phone);
+            Console.WriteLine("Кол-во заказов: " + client_1.spisokRequest);
+            Console.WriteLine("Общая сумма заказов: " + client_1.totalCountRequest);
+            Console.WriteLine("");
+
+            //Структура товара
+            RequestItem requestItem_1 = new RequestItem { article = article, count_article = 5 };
+            Console.WriteLine("Структура товара: ");
+            Console.WriteLine("Товар: " + requestItem_1.article.name);
+            Console.WriteLine("Кол-во товаров: " + requestItem_1.count_article);
+            Console.WriteLine("");
+
+            //Структура заказа
+            Request request_1 = new Request
+            {
+                code_requestItem = 6,
+                client = client_1,
+                date_request = "23.03.2026",
+                items = new RequestItem[] { requestItem_1 },
+                //sum_request
+            };
+            Console.WriteLine("Структура заказа");
+            Console.WriteLine("Код: " + request_1.code_requestItem);
+            Console.WriteLine("Клиент: " + request_1.client.fullName);
+            Console.WriteLine("Дата заказа: " + request_1.date_request);
+            Console.WriteLine("Перечень заказанных товаров:");
+            foreach (RequestItem item in request_1.items)
+            {
+                Console.WriteLine(item.count_article);
+            }
+            Console.WriteLine("Сумма заказа: " + request_1.sum_request);
+        }
+    }
+}
+
---
+++ 15.05-3/8.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace ConsoleNew
+{
+    public class Student
+    {
+        private string surname;
+        private string name;
+        private string patronymic; //Отчество
+        private string group;
+        private int age;
+        private int[][] grades; //Зубчатый
+
+        //Конструктор с параметрами
+        public Student(string surname, string name, string patronymic, string group, int age)
+        {
+            //Присвоение
+            this.surname = surname;
+            this.name = name;
+            this.patronymic = patronymic;
+            this.group = group;
+            this.age = age;
+
+            //Массив
+            grades = new int[3][];
+            grades[0] = new int[10];
+            grades[1] = new int[10];
+            grades[2] = new int[10];
+
+            //Заполнение массива единицой
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    grades[i][j] = -1;
+                }
+            }
+        }
+
+        //Установка оценки
+        public void setGrades(int subject, int index, int grade)
+        {
+            if (subject < 0 || subject > 2)
+            {
+                Console.WriteLine("Неверный предмет!");
+                return;
+            }
+            if (index < 0 || index > 9)
+            {
+                Console.WriteLine("Неверный индекс!");
+                return;
+            }
+            if (grade < 1 || grade > 5)
+            {
+                Console.WriteLine("Неверная оценка!");
+                return;
+            }
+            grades[subject][index] = grade;
+        }
+
+        //Получение оценки
+        public int getGrades(int subject, int index)
+        {
+            if (subject < 0 || subject > 2)
+            {
+                return -1;
+            }
+            if (index < 0 || index > 9)
+            {
+                return -1;
+            }
+            return grades[subject][index];
+        }
+
+        //Средний балл
+        public double getAvg(int subject)
+        {
+            if (subject < 0 || subject > 2)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (grades[subject][i] != -1)
+                {
+                    sum += grades[subject][i];
+                    count++;
+                }
+            }
+            return sum / count;
+        }
+
+        //Вывод
+        public void getInfo()
+        {
+            Console.WriteLine("Фамилия: " + surname);
+            Console.WriteLine("Имя: " + name);
+            Console.WriteLine("Отчество: " + patronymic);
+            Console.WriteLine("Группа: " + group);
+            Console.WriteLine("Возраст: " + age);
+            Console.WriteLine("Оценки: " + grades);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объект
+            Student student_1 = new Student("Иванов", "Иван", "Иванович", "РПО 24/2", 17);
+
+            //Установка оценок
+            student_1.setGrades(4, 5, 2);
+
+            //Вывод оценки
+            student_1.getGrades(0, 0);
+
+            //Вывод
+            student_1.getInfo();
+        }
+    }
+}
